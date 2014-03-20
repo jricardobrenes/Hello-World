0bdcddeb15c02dee97794262ab74042955f0e6d6
jricardobrenes <jricardobrenes@gmail.com>
2014-03-19 20:51:02 -0600

    Create Prueba.c

---
+++ Prueba.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Instituto Tecnológico de Costa Rica
+ * Escuela de Ingeniería en Computación
+ * Lenguajes de Programación
+ * Prof. Eddy Ramírez
+ * Estudiantes: 
+ *	José Ricardo Brenes Camacho(201236179)
+ *	Melvin Sandoval Quiros(201240810)
+ *
+ */
+
+int main(int argc, char* argv[]){
+  printf("Prueba Laberinto\n");
+  printf(nueva.valor+"\n");
+  return 0;
+}
